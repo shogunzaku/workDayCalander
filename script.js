12af346044c1cf917ab87d7ac87217bfc48f79ce
Shogun <shogunzaku@gmail.com>
2022-03-27 18:27:37 -0700

    set up time blocks

---
+++ script.js
@@ -0,0 +1,23 @@
+var $timeBlocks = $(".time-block");
+var currentHour = moment().format("H");
+
+
+function setUpTimeBlocks(){
+    $timeBlocks.each(function(){
+      var $thisBlock = $(this);
+      var thisBlockHr = parseInt($thisBlock.attr("data-hour"));
+
+      //add style to time blocks to show where we are in the day
+      if (thisBlockHr == currentHour) {
+        $thisBlock.addClass("present").removeClass("past future");
+      }
+      if (thisBlockHr < currentHour) {
+        $thisBlock.addClass("past").removeClass("present future");
+      }
+      if (thisBlockHr > currentHour) {
+        $thisBlock.addClass("future").removeClass("past present");
+      }
+    });
+}
+// set in time blocks 
+setUpTimeBlocks();
